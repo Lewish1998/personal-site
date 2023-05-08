09f9855abc27977db210c15d0e19f658bdf2c529
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-05-08 14:44:25 +0100

    water animation - not responsive

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -12,72 +12,11 @@ const Landing = ({ name }) => {
 
         <div className="description">
 
-      {/* test */}
+      <div className="content">
+        <h1>JUNIOR SOFTWARE DEVELOPER</h1>
+        <h1 id="htwo">JUNIOR SOFTWARE DEVELOPER</h1>
+      </div>
 
-        <svg viewbox="0 0 100 20">
-          <defs>
-              <linearGradient id="gradient">
-                <stop color="#00d3e3"/>
-              </linearGradient>
-            <pattern 
-            id="wave" 
-            x="0" y="-0" 
-            width="100" 
-            height="100%" 
-            patternUnits="userSpaceOnUse">
-              <path 
-                id="wavePath" 
-                d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" 
-                mask="url(#mask)" 
-                // fill="url(#gradient)"> 
-                fill="#00d3e3"> 
-                <animateTransform
-                    attributeName="transform"
-                    begin="0s"
-                    dur="1.5s"
-                    type="translate"
-                    from="0,0"
-                    to="40,0"
-                    repeatCount="indefinite" />
-              </path>
-            </pattern>
-          </defs>
-
-        <text 
-          text-anchor="middle" 
-          x="250" 
-          y="20" 
-          font-size="26" 
-          fill="white" 
-          fill-opacity="1">
-          JUNIOR SOFTWARE DEVELOPER
-        </text>
-        <text 
-          text-anchor="middle" 
-          x="250" 
-          y="20" 
-          font-size="26" 
-          fill="url(#wave)" 
-          fill-opacity="1">
-          JUNIOR SOFTWARE DEVELOPER
-        </text>
-
-        </svg>
-
-
-
-
-
-          {/* Typewriter animation */}
-          {/* <Typewriter
-            className="description"
-            onInit={(typewriter) => {
-              typewriter
-              .changeDelay(50)
-              .typeString("Junior Software Developer")
-              .start();
-            }}
-          /> */}
         </div>
       </div>
       <SocialIcons />
