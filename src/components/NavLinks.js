e7ef3c631b3b9258feb13dc235e02135a19447ec
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-05-22 19:51:44 +0100

    route adjustments

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -16,7 +16,7 @@ const NavLinks = () => {
         )}
       </button>
       <nav className={`links ${isMenuOpen ? "open" : "closed"}`}>
-        <NavLink to="/personal-site" onClick={() => setIsMenuOpen(false)}>
+        <NavLink to="/" onClick={() => setIsMenuOpen(false)}>
           Home
         </NavLink>
         <NavLink to="/about" onClick={() => setIsMenuOpen(false)}>
